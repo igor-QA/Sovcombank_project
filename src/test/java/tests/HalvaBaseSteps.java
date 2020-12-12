11dd2e746ef4e4dd9f6c288d25a2313d4a38cedd
Igor Pavlov <igortvk@ya.ru>
2020-12-12 18:58:59 +0300

    added css for button

--- src/test/java/tests/HalvaBaseSteps.java
+++ src/test/java/tests/HalvaBaseSteps.java
@@ -24,7 +24,7 @@ public class HalvaBaseSteps extends TestBase {
 
     @Step("Проверить переход на страницу заказа")
     public void checkOrderPage() {
-        $("html").waitUntil(visible, 5000).shouldHave(text("Заказать карту"));
+        $("html").waitUntil(visible, 3000).shouldHave(text("Заказать карту"));
     }
 
     @Step("Заказать карту Халва")
@@ -44,7 +44,7 @@ public class HalvaBaseSteps extends TestBase {
         $$(byText(region)).find(visible).click();
         $(byName("localityAddress")).setValue("А");
         $("#react-autowhatever-1--item-0").click();
-        $(".formBtn").click();
+        $("button.formBtn").click(); //div.formBtnOuter button
 
     }
 
