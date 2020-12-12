d6b77f982f11cf3485fe35e61bdff782e192399a
Igor Pavlov <igortvk@ya.ru>
2020-12-12 19:39:43 +0300

    refactoring random block

--- src/test/java/helpers/RandomRegions.java
+++ src/test/java/helpers/RandomRegions.java
@@ -7,8 +7,9 @@ import java.util.stream.Stream;
 
 public enum RandomRegions {
     MOSCOW("Москва и Московская область"),
-    SAINT_PETERSBURG("Санкт-Петербург и Ленинградская область"),
-    NIZHNY_NOVGOROD("Нижегородская область"),
+    //SAINT_PETERSBURG("Санкт-Петербург и Ленинградская область"),
+    ASTRAHAN("Астраханская область"),
+    //NIZHNY_NOVGOROD("Нижегородская область"),
     TATARSTAN("Республика Татарстан");
 
     private final String name;
